3bc11b9edd2c8d2dde92b35e9ed0f8de947bfeb7
Tripp Lyons <tripplyons@gmail.com>
2022-11-05 16:00:38 -0400

    Move whisper files

--- flask.py
+++ flask.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask import request
-import json 
+import json
+from whisper_functions import transcribe, download
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/t_url", methods=['POST'])
+
+@app.route("/t_url", methods=['GET'])
 def do_transcription():
-    d = request.data
-    d['url'] = 
-    
-@app.route("/t_file", methods=['POST'])
-def do_transcription():
-    d = request.data
-    d['file'] = 
+    args = request.args
+    url = args.get('url')
+
+    download(url, 'tmp/tmp.wav')
+    text = transcribe('tmp/tmp.wav')
+
+    return text
+
+
+# @app.route("/t_file", methods=['POST'])
+# def do_transcription():
+#     d = request.data
+#     d['file'] =
