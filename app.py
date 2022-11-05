955430448c1b206810a0c9d446efd70cc301e766
CKegel <57967583+EggBagel@users.noreply.github.com>
2022-11-05 16:26:29 -0400

    added file upload screen

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import flask
 from flask import Flask, flash, request, redirect, url_for, request
 from werkzeug.utils import secure_filename
@@ -50,3 +51,38 @@ def upload_file():
 def do_file_transcription():
     d = request.data
     d['file'] = {''}
+=======
+import flask
+from flask import Flask, flash, request, redirect, url_for, request
+from werkzeug.utils import secure_filename
+import json
+import os
+from whisper_functions import download, transcribe
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './upload_temp/'
+ALLOWED_EXTENSIONS = {'mov', 'mp4', 'webm', 'mkv'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/t_url", methods=['GET'])
+def do_transcription():
+    args = request.args
+    url = args.get('url')
+
+    unique = hash(url)
+
+    path = 'tmp/' + str(unique) + '.wav'
+
+    download(url, path)
+    text = transcribe(path)
+
+    os.remove(path)
+
+    return text, 200, {'Content-Type': 'text/plain; charset=utf-8'}
