ff2a858896d242e470e3730193d3369e996c9e3e
CKegel <57967583+EggBagel@users.noreply.github.com>
2022-11-05 16:58:32 -0400

    Added file upload form

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import os
 from whisper_functions import download, transcribe
 
 app = Flask(__name__)
-UPLOAD_FOLDER = './upload_temp/'
-ALLOWED_EXTENSIONS = {'mov', 'mp4','webm', 'mkv'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
@@ -31,30 +27,20 @@ def do_link_transcription():
 
     return text, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
+
 @app.route("/upload_file", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+        #f.save(secure_filename(f.filename))
+        return "file uploaded sucessfully"
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
+    <form action="http://localhost:5000/upload_file" method=POST enctype = "multipart/form-data">
       <input type=file name=file>
-      <input type=submit value=Upload>
+      <input type=submit>
     </form>
     '''
 
