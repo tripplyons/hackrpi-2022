0792681b8c39426f24468169a57715e587d19273
John Shaughnessy <25235590+mrmola@users.noreply.github.com>
2022-11-05 17:02:06 -0400

    Webpage 2

---
+++ webpage/js/main.js
@@ -0,0 +1,54 @@
+inputDiv   = document.getElementById('uploadBox');
+linkInput  = document.getElementById('linkInput');
+
+linkInput.addEventListener('keydown', function(e){
+    console.log(e.key);
+    if(e.key == 'Enter'){
+        console.log(e)
+        loading();
+        fetch("/t_url?url="+linkDiv.value, {
+            "credentials": "omit",
+            "method": "GET",
+            "mode": "cors"
+        }).then(function(e) {
+            e.text().then(f=>{
+                setCaption(e);
+            })
+            stopLoading();
+        });
+    }
+})
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+    console.log("bruh")
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+  
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+        [...ev.dataTransfer.items].forEach((item, i) => {
+            // If dropped items aren't files, reject them
+            if (item.kind === 'file') {
+                const file = item.getAsFile();
+                console.log(file);
+            }
+        })
+    }
+}
+
+function setCaption(e) {
+
+}
+function loading() {
+
+}
+function stopLoading() {
+
+}
