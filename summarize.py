dcec138385eafdf4c29e8fea240aeb271f42b334
Tripp Lyons <tripplyons@gmail.com>
2022-11-05 22:37:44 -0400

    Better generation

--- summarize.py
+++ summarize.py
@@ -28,36 +28,40 @@ co = cohere.Client('Rwi47oVJlWOajOUfjnaLuqLcGwZpPMhciWAmcZiu')
 
 
 def summarize_single(text, temperature=0.0):
-    response = co.generate(
-        model='xlarge',
-        prompt="""Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn't the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to "the dusty section of the dictionary" to find its latest words.
+    try:
+        response = co.generate(
+            model='xlarge',
+            prompt="""Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn't the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to "the dusty section of the dictionary" to find its latest words.
 
-TLDR: Wordle has not gotten more difficult to solve.
---
-Passage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.
+    In Summary: Wordle has not gotten more difficult to solve.
+    --
+    Passage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.
 
-TLDR: ArtificialIvan has raised $190 million in Series C funding.
---
-Passage: """ + text + "\n\nTLDR:",
-        max_tokens=300,
-        temperature=temperature,
-        stop_sequences=["\n"])
+    In Summary: ArtificialIvan has raised $190 million in Series C funding.
+    --
+    Passage: """ + text + "\n\nIn Summary:",
+            max_tokens=300,
+            temperature=temperature,
+            stop_sequences=["\n"])
 
-    summary = response.generations[0].text
-    # remove space and stop sequence
-    summary = summary[:-2].strip()
+        summary = response.generations[0].text
+        # remove space and stop sequence
+        summary = summary[:-2].strip()
 
-    if len(summary) > 200:
-        print('RETRYING BECAUSE OF ' + str(len(summary)) +
-              ' LENGTH' + ' ' + str(temperature))
-        return summarize_single(text, temperature=temperature + 0.1)
-    return summary
+        if len(summary) > 500:
+            print('RETRYING BECAUSE OF ' + str(len(summary)) +
+                  ' LENGTH' + ' ' + str(temperature))
+            return summarize_single(text, temperature=temperature + 0.1)
+        return summary
+    except:
+        pass
+    return summarize_single(text, temperature=temperature)
 
 
 def group_sentences(text):
     sentences = text.split(". ")
 
-    sentences_per_paragraph = 8
+    sentences_per_paragraph = 5
 
     if len(sentences) < sentences_per_paragraph:
         return [text]
