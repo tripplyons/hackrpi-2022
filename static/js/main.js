075a6bd7a45a592bbe1988d4b61a98bc5c577a2f
CKegel <57967583+EggBagel@users.noreply.github.com>
2022-11-05 21:45:50 -0400

    modal disable

--- static/js/main.js
+++ static/js/main.js
@@ -6,8 +6,11 @@ resultsDisplay = document.getElementById('resultsDisplay');
 resultsDisplay.style.display = "none";
 selectionInterface = document.getElementById('selectionInterface');
 detailsModal = document.getElementById('DetailedResultsModal');
-closeSpan = document.getElementsById("closeModal");
+closeSpan = document.getElementById("closeSpan");
 
+closeSpan.onclick = function() {
+  detailsModal.style.display = "none";
+}
 
 linkInput.addEventListener('keydown', uploadLink)
 goButton.onclick = uploadLink;
